f96939de398dc604ab7ace0352dbca2bb7744004
kumaratulbatham <kumaratulbatham@gmail.com>
2019-09-23 16:06:51 +0530

    Use Of Map and Set

--- src/app/layout/screen2/screen2.component.ts
+++ src/app/layout/screen2/screen2.component.ts
@@ -6,16 +6,17 @@ export interface PeriodicElement {
   position: number;
   weight: number;
   symbol: string;
+  date?: any;
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
-  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
-  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-  { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
-  { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
-  { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' }
+  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H', date: Date.now() },
+  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He', date: Date.now() },
+  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li', date: Date.now()},
+  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be', date: Date.now()},
+  { position: 5, name: 'Boron', weight: 10.811, symbol: 'B', date: Date.now()},
+  { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C', date: Date.now()},
+  { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N', date: Date.now()}
 ];
 
 @Component({
@@ -24,12 +25,28 @@ const ELEMENT_DATA: PeriodicElement[] = [
   styleUrls: ['./screen2.component.scss']
 })
 export class Screen2Component implements OnInit {
-  displayedColumns = ['position', 'name', 'weight', 'symbol'];
-    dataSource = new MatTableDataSource(ELEMENT_DATA);
+  displayedColumns = ['position', 'name', 'weight', 'symbol', 'date'];
+  dataSource = new MatTableDataSource(ELEMENT_DATA);
 
   constructor() { }
 
   ngOnInit() {
+    console.log(this.dataSource)
+  }
+
+  /** Map and Set Data Structure in Javascript */
+  getArrrayDetails() {
+    let arrayData = [2, 2, 3, 8, 6, 8, 7, 9, 5, 1, 6];
+    var setData = new Set(arrayData);
+    console.log('setData:', '----------',setData);
+    var mapData = new Map();
+    var keyString = 'a string',
+    keyObj = {},
+    keyFunc = function () { };
+    mapData.set(keyString, "value associated with 'a string'");
+    mapData.set(keyObj, 'value associated with keyObj');
+    mapData.set(keyFunc, 'value associated with keyFunc');
+    console.log('mapData:', '----------',mapData);
   }
 
 }
