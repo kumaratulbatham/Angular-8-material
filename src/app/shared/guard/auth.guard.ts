ff2c02a21c279627f0fc99b688fa97bc74bfd08f
kumaratulbatham <kumaratulbatham@gmail.com>
2019-10-15 17:54:00 +0530

    Add Authgaurd intercepter

--- src/app/shared/guard/auth.guard.ts
+++ src/app/shared/guard/auth.guard.ts
@@ -1,16 +1,33 @@
 import { Injectable } from '@angular/core';
-import { CanActivate } from '@angular/router';
-import { Router } from '@angular/router';
+import {
+    CanActivate,
+    ActivatedRouteSnapshot,
+    RouterStateSnapshot,
+    Router
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthenticationService } from '../services/authentication.service';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-    constructor(private router: Router) {}
+    constructor(private router: Router, private authService: AuthenticationService) { }
 
-    canActivate() {
-        if (localStorage.getItem('isLoggedin')) {
+    canActivate(
+        route: ActivatedRouteSnapshot,
+        state: RouterStateSnapshot): boolean | Observable<boolean> | Promise<boolean> {
+        const currentUser = this.authService.currentUserValue;
+        if (currentUser) {
+            // check if route is restricted by role
+            if (route.data.roles && route.data.roles.indexOf(currentUser.assignedRoles) === -1) {
+                // role not authorised so redirect to home page
+                this.router.navigate(['/']);
+                return false;
+            }
+            //  authorised so return true
             return true;
         }
-
+        // not logged in so redirect to login page with the return url
+        //this.router.navigate(['/login'], { queryParams: { returnUrl: state.url } });
         this.router.navigate(['/login']);
         return false;
     }
